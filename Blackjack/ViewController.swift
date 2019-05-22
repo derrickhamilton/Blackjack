baffe88dcb70db56ccf90d7fe225cff49f6f4f01
derrickhamilton <derrickhamilton212@gmail.com>
2019-05-21 19:18:44 -0700

    added images for cards and added some stuff to the view

--- Blackjack/ViewController.swift
+++ Blackjack/ViewController.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var gameModel = BlackjackModel()
+    var playerHandView: UIStackView!
+    var dealerHandView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        playerHandView = view.viewWithTag(1) as! UIStackView
+        dealerHandView = view.viewWithTag(2) as! UIStackView
+        /*let image: UIImage = UIImage(named: "ace_of_spades")!
+        let image2: UIImage = UIImage(named: "ace_of_clubs")!
+        let imageView: UIImageView = UIImageView(frame: CGRect(x:0, y:0, width:60, height:90))
+        imageView.image = image
+        stackView.addSubview(imageView)
+        let imageView2: UIImageView = UIImageView(frame: CGRect(x:0, y:0, width:60, height:90))
+        imageView2.image = image2
+        stackView2.addSubview(imageView2)*/
     }
 
     override func didReceiveMemoryWarning() {
